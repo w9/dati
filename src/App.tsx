b71db5dd0f7019a819e0fdac4983d61a7fe18de4
Xun Zhu <zhuxun2@gmail.com>
2017-09-24 20:41:29 -1000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,122 @@
 import * as React from 'react';
 import './App.css';
 import * as PIXI from 'pixi.js';
+// import * as update from 'immutability-helper';
 // import { normal } from 'jStat';
 // import { range } from './lib/helper';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component {
-  canvas: HTMLDivElement | null;
+interface Point {
+  x: number;
+  y: number;
+  group?: number;
+}
 
-  colorPalette: string[] = ['#db2d20', '#01a0e4', '#01a252', '#a16a94', '#222222', '#b5e4f4'];
+interface AppState {
+  points: Point[];
+}
 
-  constructor() {
-    super();
-  }
+interface AppProps {
+  state: 'drawing' | 'box-selecting';
+  initalPoints: Point[];
+  onStateChange: (state: AppState) => any;
+}
 
-  componentDidMount() {
-    const graphics = new PIXI.Graphics();
-    graphics.lineStyle(0);
-    graphics.beginFill(0xffff0b, 0.5);
-    graphics.drawCircle(0, 0, 5);
-    graphics.endFill();
 
-    const app = new PIXI.Application({ width: 800, height: 800, backgroundColor: 0x000000 });
-    if (this.canvas !== null) {
-      console.log(this.canvas);
-      this.canvas.appendChild(app.view);
-    }
+class App extends React.Component<AppProps, {}> {
+  // private static readonly COLOR_PALETTE: string[] =
+  //   ['#db2d20', '#01a0e4', '#01a252', '#a16a94', '#222222', '#b5e4f4'];
 
-    const squareTexture = app.renderer.generateTexture(graphics, 1);
+  private canvas: HTMLDivElement | null;
+  private dotG: PIXI.Graphics;
+  private squareT: PIXI.Texture;
+  private container: PIXI.Container;
+  private app: PIXI.Application;
 
-    // for (const _ of range(10000)) {
-    //   const square = new PIXI.Sprite(squareTexture);
-    //   square.x = normal.sample(400, 100);
-    //   square.y = normal.sample(400, 100);
-    //   // square.x = (Math.random() - 0.5) * 700 + 400;
-    //   // square.y = (Math.random() - 0.5) * 700 + 400;
-    //   square.anchor.set(0.5);
+  // We use an alternative state variable to by pass the default React Component
+  // life cycle. We don't want, e.g., batched-update.
+  private _state: AppState = {
+    points: [],
+  };
 
-    //   // app.ticker.add((delta) => {
-    //   //   square.rotation += 0.1 * delta;
-    //   // });
+  public constructor(props: AppProps) {
+    super(props);
 
-    //   app.stage.addChild(square);
-    // }
+    this.app = new PIXI.Application({ width: 800, height: 800, backgroundColor: 0x000000 });
+    this.dotG = new PIXI.Graphics();
+    this.dotG.lineStyle(0);
+    this.dotG.beginFill(0xffff0b, 0.5);
+    this.dotG.drawCircle(0, 0, 5);
+    this.dotG.endFill();
 
-    app.stage.interactive = true;
-    app.stage.buttonMode = true;
-    app.stage.hitArea = new PIXI.Rectangle(0, 0, 800, 800);
-    app.stage.on('pointertap', (e) => {
-      console.log(e.data.global);
-      const square = new PIXI.Sprite(squareTexture);
+    this.squareT = this.app.renderer.generateTexture(this.dotG, 1);
+
+    this.container = new PIXI.Container();
+    this.container.x = 0;
+    this.container.y = 0;
+    this.container.interactive = true;
+    this.container.buttonMode = true;
+    this.container.hitArea = new PIXI.Rectangle(0, 0, 800, 800);
+    this.container.on('pointertap', (e) => {
+      const square = new PIXI.Sprite(this.squareT);
       square.x = e.data.global.x;
       square.y = e.data.global.y;
       square.anchor.set(0.5);
-
-      // app.ticker.add((delta) => {
-      //   square.rotation += 0.1 * delta;
-      // });
-
-      app.stage.addChild(square);
+      this.container.addChild(square);
+      this._state.points.push({ x: square.x, y: square.y });
+      this.props.onStateChange(this._state);
     });
+
+    this.app.stage.addChild(this.container);
+
+    this.drawInitialPoints();
   }
 
-  render() {
+  public componentDidMount() {
+    if (this.canvas !== null) {
+      this.canvas.appendChild(this.app.view);
+    } else {
+      console.error('The canvas element is not found!');
+    }
+  }
+
+  private drawInitialPoints() {
+    if (this.props.initalPoints) {
+      this._state.points = this.props.initalPoints;
+      this.addPoints(this._state.points);
+    } else {
+      this._state.points = [];
+    }
+  }
+
+  private addPoints(points: Point[]) {
+    for (let p of points) {
+      const square = new PIXI.Sprite(this.squareT);
+      square.x = p.x;
+      square.y = p.y;
+      square.anchor.set(0.5);
+      this.container.addChild(square);
+    }
+  }
+
+  public componentWillReceiveProps(nextProps: AppProps) {
+    // TODO
+  }
+
+  public shouldComponentUpdate() {
+    return false;
+  }
+
+  public render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Dati React Component</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          Enjoy playing!
         </p>
         <div ref={(x) => this.canvas = x} />
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,17 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { range } from './lib/helper';
+import { normal } from 'jStat';
+
+
+const initialPoints = [];
+for (let _ of range(100)) {
+  initialPoints.push({ x: normal.sample(400, 100), y: normal.sample(400, 100) });
+}
 
 ReactDOM.render(
-  <App />,
+  <App state="drawing" onStateChange={(e) => console.log(e)} initalPoints={initialPoints} />,
   document.getElementById('root') as HTMLElement,
 );
 
